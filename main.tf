d76fbcaadf7726c58810ad5df6bf428996d4a1c5
Inatan Hertzog <inatan.hertzog@gmail.com>
2023-03-15 15:49:00 -0300

    test user_data

--- main.tf
+++ main.tf
@@ -15,9 +15,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0054772d386e00cfc"
+  ami           = "ami-04473c3d3be6a927f"
   instance_type = "t2.micro"
-
+  key_name = "iac-ina"
+  user_data = <<-EOF
+                #!/bin/bash
+                cd /home/ubuntu
+                echo “<h1>Mensagem a ser mostrada</h1>” > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
   tags = {
     Name = "Terraform hello world"
   }
